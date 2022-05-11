95b4d9b85c2c0cec0ef8a28215e8e7bd839b3b87
mateus.melo9@gmail.com <901223280401ma>
2022-05-11 15:03:37 -0300

    Data base insert

---
+++ src/main/java/com/devmelo/hruser/domain/Role.java
@@ -0,0 +1,27 @@
+package com.devmelo.hruser.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+@Entity(name = "tb_role")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Role implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roleName;
+
+
+}
